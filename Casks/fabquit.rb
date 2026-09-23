61af091b43900f77c8574a6fa1669585f1ab62a5
fabburst <fabimacg5@gmail.com>
2026-09-23 17:54:44 +0200

    fabquit 0.2.0

---
+++ Casks/fabquit.rb
@@ -0,0 +1,26 @@
+cask "fabquit" do
+  version "0.2.0"
+  sha256 "da5160124291b1794675c0aad19f50f1190c21810795fc3722b3a859fdbcaeef"
+
+  url "https://github.com/fabburst/FabQuit/releases/download/v#{version}/FabQuit-v#{version}.zip"
+  name "FabQuit"
+  desc "Quit every running app at once, except the ones you keep"
+  homepage "https://github.com/fabburst/FabQuit"
+
+  auto_updates true
+  depends_on macos: ">= :sonoma"
+
+  app "FabQuit.app"
+
+  # Not notarized: remove the quarantine flag so Gatekeeper lets it open.
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-d", "-r", "com.apple.quarantine", "#{appdir}/FabQuit.app"]
+  end
+
+  zap trash: [
+    "~/Library/Preferences/io.github.fabburst.fabquit.plist",
+    "~/Library/Caches/io.github.fabburst.fabquit",
+    "~/Library/HTTPStorages/io.github.fabburst.fabquit",
+  ]
+end
